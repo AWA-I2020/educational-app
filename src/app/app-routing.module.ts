d5b54d1ce6624411a051e058cbd1b1318c171ed8
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2020-06-29 18:35:45 -0400

    routing and components added to app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,38 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+import { SignInComponent } from "./authentication/sign-in/sign-in.component";
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: "",
+    redirectTo: "sign-in",
+    pathMatch: "full",
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: "teacher",
+    loadChildren: () =>
+      import("./teacher/teacher.module").then((m) => m.TeacherPageModule),
+  },
+  {
+    path: "student",
+    loadChildren: () =>
+      import("./student/student.module").then((m) => m.StudentPageModule),
+  },
+  {
+    path: "parent",
+    loadChildren: () =>
+      import("./parent/parent.module").then((m) => m.ParentPageModule),
+  },
+  {
+    path: "sign-in",
+    component: SignInComponent,
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
