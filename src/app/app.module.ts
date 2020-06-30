986d81c3cd14ffab0e1319b64e443b78ba3ff0a1
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2020-06-29 23:49:22 -0400

    cripto module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { StatusBar } from "@ionic-native/status-bar/ngx";
 
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
 import { SignInComponent } from "./authentication/sign-in/sign-in.component";
 import { SignUpComponent } from "./authentication/sign-up/sign-up.component";
 import { ServiceWorkerModule } from "@angular/service-worker";
@@ -22,6 +24,8 @@ import { ReactiveFormsModule, FormsModule } from "@angular/forms";
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
     ServiceWorkerModule.register("ngsw-worker.js", {
       enabled: environment.production,
     }),
