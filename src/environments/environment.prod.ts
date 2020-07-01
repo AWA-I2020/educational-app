f4017ae20ca3865495e825e1223cd40aa78d65dc
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2020-06-30 22:28:09 -0400

    firebase config added to production enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCpw3RSxqYQE6JO0sq95jQv0FIjdi40t2c",
+    authDomain: "educational-app-f6e2a.firebaseapp.com",
+    databaseURL: "https://educational-app-f6e2a.firebaseio.com",
+    projectId: "educational-app-f6e2a",
+    storageBucket: "educational-app-f6e2a.appspot.com",
+    messagingSenderId: "151728548955",
+    appId: "1:151728548955:web:7213f5ecef374575a2a37e",
+    measurementId: "G-3HW6BW9B7Q",
+  },
 };
