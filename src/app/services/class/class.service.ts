6898c66255da170a2f3003d6c4662161965405a5
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2020-07-02 12:36:17 -0400

    class service with models added

---
+++ src/app/services/class/class.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from "@angular/core";
+import { Class } from "src/app/models/class";
+import { map, take } from "rxjs/operators";
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  DocumentReference,
+} from "@angular/fire/firestore";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ClassService {
+  private classCollection: AngularFirestoreCollection<Class>;
+  constructor(private afs: AngularFirestore) {
+    this.classCollection = this.afs.collection<Class>("classes");
+  }
+
+  addClass(data: Class): Promise<DocumentReference> {
+    return this.classCollection.add(data);
+  }
+
+  getClass(id: string): Observable<Class> {
+    let classDoc = this.afs.doc<Class>(`classes/${id}`);
+    return classDoc.valueChanges().pipe(
+      take(1),
+      map((collection) => {
+        return collection;
+      })
+    );
+  }
+
+  deleteClass(id: string): Promise<void> {
+    return this.classCollection.doc(id).delete();
+  }
+}
