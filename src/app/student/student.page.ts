e05e771a1aaa83e335662b1d776a7fcdd32a2b23
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2020-07-02 22:26:30 -0400

    student page example

--- src/app/student/student.page.ts
+++ src/app/student/student.page.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ProfileComponent } from "../shared/profile/profile.component";
+import { PopoverController } from "@ionic/angular";
+import { Class } from "../models/class";
 
 @Component({
-  selector: 'app-student',
-  templateUrl: './student.page.html',
-  styleUrls: ['./student.page.scss'],
+  selector: "app-student",
+  templateUrl: "./student.page.html",
+  styleUrls: ["./student.page.scss"],
 })
 export class StudentPage implements OnInit {
+  classes: Class[] = [
+    {
+      subject: "Matematicas",
+      grade: 2,
+      parallel: "B",
+      teacher_id: "dasfbasi",
+      id: "safasd",
+    },
+  ];
+  constructor(private popoverController: PopoverController) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  async showOptions(ev) {
+    const popover = await this.popoverController.create({
+      component: ProfileComponent,
+      event: ev,
+      translucent: true,
+    });
+    return await popover.present();
   }
 
+  openClass(id: string) {
+    console.log("open class", id);
+  }
 }
