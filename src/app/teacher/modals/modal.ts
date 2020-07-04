29f86d7290b640d6d1f17e131b335001285bcc4d
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2020-07-04 15:33:34 -0400

    new methods for modal

--- src/app/teacher/modals/modal.ts
+++ src/app/teacher/modals/modal.ts
@@ -1,16 +1,25 @@
-import { ModalController } from '@ionic/angular';
-import { Input, OnInit } from '@angular/core';
+import { ModalController, ToastController } from "@ionic/angular";
+import { Input, OnInit } from "@angular/core";
+import { NgxIndexedDBService } from 'ngx-indexed-db';
 
 export class Modal implements OnInit {
+  @Input() modalCtrl: ModalController;
+  @Input() toastController: ToastController;
+  @Input() indexedDbService: NgxIndexedDBService;
 
-    @Input() modalCtrl: ModalController;
+  ngOnInit() {}
 
-    ngOnInit() {
-    }
+  dismiss() {
+    this.modalCtrl.dismiss({
+      dismissed: true,
+    });
+  }
 
-    dismiss() {
-        this.modalCtrl.dismiss({
-            'dismissed': true
-        });
-    }
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+    });
+    toast.present();
+  }
 }
