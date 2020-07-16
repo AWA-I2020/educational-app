5f46c7fc11f45e3f041db9a5016205b167a3eade
CristopherSs <alanss2907@gmail.com>
2020-07-16 16:42:02 -0500

    profile added to any user

--- src/app/shared/profile/profile.component.ts
+++ src/app/shared/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { User } from 'firebase';
+import { User } from 'src/app/models/user';
+import { PopoverController, ToastController } from '@ionic/angular';
 
 
 @Component({
@@ -11,8 +12,29 @@ export class ProfileComponent implements OnInit {
 
   @Input() user: User;
 
-  constructor() { }
+  constructor(private toastController: ToastController, private popover: PopoverController) { }
 
-  ngOnInit(){}
- 
+  ngOnInit() { }
+
+  copyCode() {
+    document.addEventListener("copy", (e: ClipboardEvent) => {
+      e.clipboardData.setData("text/plain", this.user.id);
+      e.preventDefault();
+      document.removeEventListener("copy", null);
+    });
+    document.execCommand("copy");
+    this.presentToast("Codigo copiado al portapapeles");
+  }
+
+  closePopover() {
+    this.popover.dismiss();
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+    });
+    toast.present();
+  }
 }
