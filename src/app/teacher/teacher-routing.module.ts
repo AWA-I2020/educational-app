c8f77cd117da8ec1f8f9ead1ec3237dd4724aabf
CristopherSs <alanss2907@gmail.com>
2020-07-16 17:03:27 -0500

    refactor

--- src/app/teacher/teacher-routing.module.ts
+++ src/app/teacher/teacher-routing.module.ts
@@ -3,16 +3,11 @@ import { Routes, RouterModule } from "@angular/router";
 
 import { TeacherPage } from "./teacher.page";
 import { ClassComponent } from "../shared/class/class.component";
-import { ProfileComponent } from '../shared/profile/profile.component';
 
 const routes: Routes = [
   {
     path: "",
     component: TeacherPage,
-  },
-  {
-    path: "profile",
-    component: ProfileComponent
   },
   {
     path: "class/:id",
