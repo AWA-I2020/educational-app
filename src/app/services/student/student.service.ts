58769030765765f0d77815b4cf9107502a4d9d0d
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2020-07-23 23:10:17 -0400

    services updated

--- src/app/services/student/student.service.ts
+++ src/app/services/student/student.service.ts
@@ -8,6 +8,9 @@ import {
 import { Observable } from "rxjs";
 import { ClassStudent } from "src/app/models/class-student";
 import { StudentActivity } from "src/app/models/student-activity";
+import { AngularFireStorage } from "@angular/fire/storage";
+import { FileUpload } from "src/app/models/file";
+import { finalize, take, map } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root",
@@ -18,7 +21,10 @@ export class StudentService {
     StudentActivity
   >;
 
-  constructor(private afs: AngularFirestore) {
+  constructor(
+    private afs: AngularFirestore,
+    private storage: AngularFireStorage
+  ) {
     this.classesStudentsCollection = this.afs.collection<ClassStudent>(
       "classes-students"
     );
@@ -43,11 +49,50 @@ export class StudentService {
     return this.studentActivitiesCollection.add(data);
   }
 
-  getActivities(id: string): Observable<StudentActivity[]> {
+  getActivitiesOfStudent(id: string): Observable<StudentActivity[]> {
     return this.afs
       .collection<StudentActivity>("students-activities", (ref) =>
         ref.where("activity_id", "==", id)
       )
       .valueChanges();
+  }
+
+  getActivity(
+    studentId: string,
+    activityId: string
+  ): Observable<StudentActivity[]> {
+    return this.afs
+      .collection<StudentActivity>("students-activities", (ref) =>
+        ref
+          .where("activity_id", "==", activityId)
+          .where("student_id", "==", studentId)
+      )
+      .valueChanges()
+      .pipe(
+        take(1),
+        map((data) => {
+          return data;
+        })
+      );
+  }
+
+  uploadActivity(file: FileUpload): Promise<FileUpload> {
+    return new Promise((resolve, reject) => {
+      let data: FileUpload;
+      const filePath = `activities/${file.file.name}`;
+      const ref = this.storage.ref(filePath);
+      const task = this.storage.upload(filePath, file.file);
+      task
+        .snapshotChanges()
+        .pipe(
+          finalize(() => {
+            ref.getDownloadURL().subscribe((url) => {
+              data = { name: file.file.name, url: url };
+              resolve(data);
+            });
+          })
+        )
+        .subscribe();
+    });
   }
 }
