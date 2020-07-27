9068f5ec607f3365b722886b92383c572b0b05bb
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2020-07-27 18:30:43 -0400

    notification service implemented

---
+++ src/app/shared/messaging/messaging.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { AngularFireMessaging } from "@angular/fire/messaging";
+import { BehaviorSubject } from "rxjs";
+
+@Injectable()
+export class MessagingService {
+  currentMessage = new BehaviorSubject(null);
+
+  constructor(private angularFireMessaging: AngularFireMessaging) {
+    this.angularFireMessaging.onMessage((payload) => {
+      console.log(payload);
+    });
+  }
+
+  requestPermission() {
+    this.angularFireMessaging.requestToken.subscribe((token) => {
+      console.log(token);
+    });
+  }
+
+  receiveMessage() {
+    this.angularFireMessaging.messages.subscribe((msg) => {
+      console.log("show message!", msg);
+      this.currentMessage.next(msg);
+    });
+  }
+}
