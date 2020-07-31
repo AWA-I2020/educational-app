94197dad9728caf97b0e30d37568af143b1e3647
CristopherSs <alanss2907@gmail.com>
2020-07-31 17:43:54 -0500

    main student page implemented

--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { StudentPageRoutingModule } from './student-routing.module';
-
 import { StudentPage } from './student.page';
+import { ProfileComponent } from '../shared/profile/profile.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    StudentPageRoutingModule
+    StudentPageRoutingModule,
+    SharedModule,
   ],
-  declarations: [StudentPage]
+  declarations: [
+    StudentPage,
+  ],
+  exports: [
+  ]
 })
-export class StudentPageModule {}
+export class StudentPageModule { }
--- src/app/student/student.page.ts
+++ src/app/student/student.page.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuController, PopoverController } from '@ionic/angular';
+import { MenuController, PopoverController, ToastController, ModalController } from '@ionic/angular';
 import { ProfileComponent } from '../shared/profile/profile.component';
 import { Class } from '../models/class';
+import { MessagingService } from '../services/messaging/messaging.service';
+import { Router } from '@angular/router';
+import { NgxIndexedDBService } from 'ngx-indexed-db';
+import { TeacherService } from '../services/teacher/teacher.service';
+import { User } from '../models/user';
 
 @Component({
   selector: "app-student",
@@ -9,7 +14,10 @@ import { Class } from '../models/class';
   styleUrls: ["./student.page.scss"],
 })
 export class StudentPage implements OnInit {
-
+  user: User;
+  page: number = 1;
+  pageTitle: string = "Mis clases";
+  modalClass;
   classes: Class[] = [
     {
       subject: "Matematicas",
@@ -19,9 +27,27 @@ export class StudentPage implements OnInit {
       id: "safasd",
     },
   ];
-  constructor(private menu: MenuController, private popoverController: PopoverController) { }
+  constructor(private menu: MenuController,
+    public modalController: ModalController,
+    private teacherService: TeacherService,
+    private indexedDbService: NgxIndexedDBService,
+    private toastController: ToastController,
+    private router: Router,
+    private messagingService: MessagingService,
+    private popoverController: PopoverController) { }
 
   ngOnInit(): void {
+  }
+
+  ionViewWillEnter() {
+    this.indexedDbService.getAll("user").then((data) => {
+      this.user = (data[0] as unknown) as User;
+      this.teacherService.getClasses(this.user.id).subscribe((classesData) => {
+        this.classes = classesData;
+      });
+      this.messagingService.requestPermission(this.user.id);
+      this.messagingService.receiveMessage();
+    });
   }
 
   async showOptions(ev) {
@@ -45,5 +71,18 @@ export class StudentPage implements OnInit {
   openCustom() {
     this.menu.enable(true, 'custom');
     this.menu.open('custom');
+  }
+
+  changePage(valuePage, pageName) {
+    this.page = valuePage;
+    this.pageTitle = pageName;
+    this.menu.close();
+  }
+
+  signOut() {
+    this.indexedDbService.clear("user").then(() => {
+      this.menu.close();
+      this.router.navigate(["sign-in"]);
+    });
   }
 }
