4910a7c2ed50ea9cbf550d3b4a0596c91c30cd9b
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2020-08-02 10:37:04 -0400

    student main view with services

--- src/app/student/student.page.ts
+++ src/app/student/student.page.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuController, PopoverController, ToastController, ModalController } from '@ionic/angular';
-import { ProfileComponent } from '../shared/profile/profile.component';
-import { Class } from '../models/class';
-import { MessagingService } from '../services/messaging/messaging.service';
-import { Router } from '@angular/router';
-import { NgxIndexedDBService } from 'ngx-indexed-db';
-import { TeacherService } from '../services/teacher/teacher.service';
-import { User } from '../models/user';
-import { RegisterClassComponent } from './modals/register-class/register-class.component';
+import { Component, OnInit } from "@angular/core";
+import {
+  MenuController,
+  ToastController,
+  ModalController,
+  LoadingController,
+} from "@ionic/angular";
+import { Class } from "../models/class";
+import { MessagingService } from "../services/messaging/messaging.service";
+import { Router } from "@angular/router";
+import { NgxIndexedDBService } from "ngx-indexed-db";
+import { User } from "../models/user";
+import { RegisterClassComponent } from "./modals/register-class/register-class.component";
+import { StudentService } from "../services/student/student.service";
+import { ClassService } from "../services/class/class.service";
 
 @Component({
   selector: "app-student",
@@ -19,36 +24,44 @@ export class StudentPage implements OnInit {
   page: number = 1;
   pageTitle: string = "Mis clases";
   modalClass;
-  classes: Class[] = [
-    {
-      subject: "Matematicas",
-      grade: 2,
-      parallel: "B",
-      teacher_id: "dasfbasi",
-      id: "safasd",
-    },
-  ];
-  constructor(private menu: MenuController,
+  classes: Class[] = [];
+  constructor(
+    private menu: MenuController,
     public modalController: ModalController,
-    private teacherService: TeacherService,
+    private studentService: StudentService,
+    private classService: ClassService,
     private indexedDbService: NgxIndexedDBService,
+    private loadingController: LoadingController,
     private toastController: ToastController,
     private router: Router,
-    private messagingService: MessagingService,
-    private popoverController: PopoverController) { }
+    private messagingService: MessagingService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   ionViewWillEnter() {
     this.indexedDbService.getAll("user").then((data) => {
       this.user = (data[0] as unknown) as User;
-      this.teacherService.getClasses(this.user.id).subscribe((classesData) => {
-        this.classes = classesData;
-      });
+      this.studentService
+        .getClassesOfStudent(this.user.id)
+        .subscribe((classesData) => {
+          if (classesData.length > 0) {
+            classesData.forEach((data) => {
+              this.classService
+                .getClass(data.class_id)
+                .subscribe((classData) => {
+                  this.classes.push(classData);
+                });
+            });
+          }
+        });
       this.messagingService.requestPermission(this.user.id);
       this.messagingService.receiveMessage();
     });
+  }
+
+  ionViewWillLeave() {
+    this.classes = [];
   }
 
   async openModal() {
@@ -58,23 +71,25 @@ export class StudentPage implements OnInit {
         modalCtrl: this.modalController,
         toastController: this.toastController,
         indexedDbService: this.indexedDbService,
+        loadingController: this.loadingController,
+        user: this.user,
       },
     });
     return await modal.present();
   }
 
   openFirst() {
-    this.menu.enable(true, 'first');
-    this.menu.open('first');
+    this.menu.enable(true, "first");
+    this.menu.open("first");
   }
 
   openEnd() {
-    this.menu.open('end');
+    this.menu.open("end");
   }
 
   openCustom() {
-    this.menu.enable(true, 'custom');
-    this.menu.open('custom');
+    this.menu.enable(true, "custom");
+    this.menu.open("custom");
   }
 
   changePage(valuePage, pageName) {
