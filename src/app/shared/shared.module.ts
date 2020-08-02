582891dae566ab40cd273600419b2aa96904a863
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2020-08-02 10:59:03 -0400

    custom schema added to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,11 @@
-import {NgModule} from '@angular/core';
-import { ProfileComponent } from './profile/profile.component';
-import { CommonModule } from '@angular/common';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { ProfileComponent } from "./profile/profile.component";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
-    imports: [
-        CommonModule
-    ],
-    declarations: [
-        ProfileComponent
-    ],
-    exports: [
-        ProfileComponent
-    ],
+  imports: [CommonModule],
+  declarations: [ProfileComponent],
+  exports: [ProfileComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class SharedModule {
-}
+export class SharedModule {}
