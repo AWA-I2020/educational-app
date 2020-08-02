c511670117a931ec4d80753c88d1559d6916f2ea
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2020-08-02 10:36:49 -0400

    register class model with services

--- src/app/student/modals/register-class/register-class.component.ts
+++ src/app/student/modals/register-class/register-class.component.ts
@@ -1,22 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { Modal } from 'src/app/shared/modal';
-import { ClassService } from 'src/app/services/class/class.service';
-import { FormGroup, FormControl, Validators,} from '@angular/forms';
+import { Component, Input } from "@angular/core";
+import { Modal } from "src/app/shared/modal";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl,
+} from "@angular/forms";
+import { StudentService } from "src/app/services/student/student.service";
+import { ClassStudent } from "src/app/models/class-student";
+import { User } from "src/app/models/user";
 
 @Component({
-  selector: 'app-register-class',
-  templateUrl: './register-class.component.html',
+  selector: "app-register-class",
+  templateUrl: "./register-class.component.html",
 })
 export class RegisterClassComponent extends Modal {
-
+  @Input() user: User;
   registerForm = new FormGroup({
-    code: new FormControl('', Validators.required),
+    code: new FormControl("", Validators.required),
   });
 
-  constructor(private classService: ClassService) {
+  constructor(private studentService: StudentService) {
     super();
   }
 
-  onSubmit() {}
+  onSubmit() {
+    this.loading("Registrandose en clase");
+    console.log(this.code.value);
+    let classStudent: ClassStudent = {
+      class_id: this.code.value,
+      student_id: this.user.id,
+      token: "",
+    };
+    this.studentService.addStudent(classStudent).then(() => {
+      this.presentToast("Te registraste en la clase");
+      this.dismissLoading();
+      this.dismiss();
+    });
+  }
 
+  get code(): AbstractControl {
+    return this.registerForm.get("code");
+  }
 }
