109e7f45e343b08596cd95c99dadcc0ee231deb0
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2020-08-02 18:27:14 -0400

    notifications component modified

--- src/app/shared/notifications/notifications/notifications.component.ts
+++ src/app/shared/notifications/notifications/notifications.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { User } from "src/app/models/user";
 import { NgxIndexedDBService } from "ngx-indexed-db";
 import { Notification } from "src/app/models/notification";
+import { Router } from "@angular/router";
+import { ToastController } from "@ionic/angular";
 
 @Component({
   selector: "app-notifications",
@@ -9,34 +11,45 @@ import { Notification } from "src/app/models/notification";
   styleUrls: ["./notifications.component.scss"],
 })
 export class NotificationsComponent implements OnInit {
-  @Input() user: User;
+  user: User;
   notifications: Notification[] = [];
-  constructor(private dbService: NgxIndexedDBService) {}
+  constructor(
+    private dbService: NgxIndexedDBService,
+    private router: Router,
+    private toastController: ToastController
+  ) {}
 
   ngOnInit() {}
 
   ionViewWillEnter() {
-    this.dbService.getAll("notifications").then((data) => {
-      if (data.length > 0) {
-        data.forEach((notificationData) => {
-          if (notificationData["user_id"] === this.user.id) {
-            let notification: Notification = {
-              body: notificationData["body"],
-              title: notificationData["title"],
-              dateReceived: new Date(notificationData["id"]),
-              read: notificationData["read"],
-              user_id: notificationData["user_id"],
-              id: notificationData["id"],
-            };
-            this.notifications.push(notification);
-          }
-        });
-      }
+    this.dbService.getAll("user").then((data) => {
+      this.user = (data[0] as unknown) as User;
+      this.dbService.getAll("notifications").then((data) => {
+        if (data.length > 0) {
+          data.forEach((notificationData) => {
+            if (notificationData["user_id"] === this.user.id) {
+              let notification: Notification = {
+                body: notificationData["body"],
+                title: notificationData["title"],
+                dateReceived: new Date(notificationData["id"]),
+                read: notificationData["read"],
+                user_id: notificationData["user_id"],
+                id: notificationData["id"],
+              };
+              this.notifications.push(notification);
+            }
+          });
+        }
+      });
     });
   }
 
   ionViewWillLeave() {
     this.notifications = [];
+  }
+
+  back() {
+    this.router.navigate(["student"]);
   }
 
   open(dateReceived: Date) {
@@ -45,15 +58,21 @@ export class NotificationsComponent implements OnInit {
       (element) => element.dateReceived === dateReceived
     );
     this.notifications[notiIndex].read = true;
-    this.dbService.update("notifications", this.notifications);
+    this.dbService.update("notifications", this.notifications[notiIndex]);
+    this.router.navigate(["student"]);
   }
 
-  delete(dateReceived: Date) {
-    console.log(dateReceived);
+  delete(id: string) {
     let notiIndex = this.notifications.findIndex(
-      (element) => element.dateReceived === dateReceived
+      (element) => element.id === id
     );
     this.notifications.splice(notiIndex, 1);
-    this.dbService.update("notifications", this.notifications);
+    this.dbService.delete("notifications", id).then(async () => {
+      const toast = await this.toastController.create({
+        message: "Notificacion eliminada.",
+        duration: 3000,
+      });
+      toast.present();
+    });
   }
 }
